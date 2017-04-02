6005cae5d9f4a1b280665310f0ce96139c193b00
LukasChen <villagelukaschen@gmail.com>
2017-04-02 23:22:22 +0800

    renamed modules

--- _javascript/app/app.module.ts
+++ _javascript/app/app.module.ts
@@ -1,14 +1,19 @@
 import * as angular from "angular";
 import * as ngSanitize from "angular-sanitize";
 import * as ngAnimate  from "angular-animate";
+
+// Juxtapose (Before / After Slider)
 import "../vendors/ng-juxtapose.min.js";
+
+// Videogular
+import "../vendors/videogular.min.js";
 
 angular
   .module("app", [
     ngSanitize,
     ngAnimate,
-    "ngJuxtapose"
-    // ngMaterial
+    "ngJuxtapose",
+    "com.2fdevs.videogular"
   ]);
 
 angular.element(document).ready(() => {
