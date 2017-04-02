03b6f98f7abc3e4db573db458f2b9cf075a8027b
LukasChen <villagelukaschen@gmail.com>
2017-04-02 16:41:58 +0800

    Added before after slider

--- _javascript/app/app.module.ts
+++ _javascript/app/app.module.ts
@@ -1,12 +1,13 @@
 import * as angular from "angular";
 import * as ngSanitize from "angular-sanitize";
 import * as ngAnimate  from "angular-animate";
-// import * as ngMaterial from "angular-material";
+import "../vendors/ng-juxtapose.min.js";
 
 angular
   .module("app", [
     ngSanitize,
-    ngAnimate
+    ngAnimate,
+    "ngJuxtapose"
     // ngMaterial
   ]);
 
